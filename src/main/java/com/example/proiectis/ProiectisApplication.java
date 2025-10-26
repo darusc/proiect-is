3796e9bb5c0f1d55c2f776b7d2dfbd94b7807fc4
hypertensiune <dariuscirneala05@gmail.com>
2025-10-26 11:37:04 +0200

    Skeleton

---
+++ src/main/java/com/example/proiectis/ProiectisApplication.java
@@ -0,0 +1,13 @@
+package com.example.proiectis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectisApplication.class, args);
+	}
+
+}

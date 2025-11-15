72e808e2ffb3d152b6e8693334eec318f9ec8e47
patrikbotarel <patrikbotarel843@gmail.com>
2025-11-15 21:11:59 +0200

    Database: repository

---
+++ src/main/java/com/example/proiectis/repository/RankingRepository.java
@@ -0,0 +1,7 @@
+package com.example.proiectis.repository;
+
+import com.example.proiectis.entity.Ranking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RankingRepository extends JpaRepository<Ranking, Long> {
+}

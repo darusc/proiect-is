3bb67f1ab51bb5c6c62028a3d9f78b096f5cca29
hypertensiune <dariuscirneala05@gmail.com>
2025-11-15 18:58:00 +0200

    game:

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,6 +1,10 @@
 class Game {
 
-    constructor(playerId) {
+    /**
+     * @param {...number} playerId
+     * @param {...function} render
+     */
+    constructor(playerId, render) {
         this.playerId = playerId;
         this.ws = new Ws(playerId, msg => this.onMessage(msg));
         this.board = null;
@@ -9,16 +13,18 @@ class Game {
         this.color = null;
         this.remainingMoves = [];
 
-        // DOM
-        this.boardDiv = document.getElementById("board");
-        this.statusDiv = document.getElementById("status");
-        this.diceDiv = document.getElementById("dice");
-        this.turnDiv = document.getElementById("turn");
+        this.render = () => {
+            render({
+                'board': this.board,
+                'dice': this.dice,
+                'remainingMoves': this.remainingMoves
+            })
+        }
     }
 
     joinRoom(room) {
         this.ws.join(room);
-        this.setStatus("Joining room " + room + "...");
+        console.log("Joining room " + room + "...");
     }
 
     onMessage(event) {
@@ -29,7 +35,6 @@ class Game {
 
             case "game_start":
                 this.color = data['payload']['white'] === this.playerId ? 87 : 66;
-                document.getElementById("playerColor").innerHTML = `Color: ${this.color === 87 ? 'White' : 'Black'}`;
                 break;
 
             case "roll_result":
@@ -38,17 +43,13 @@ class Game {
                     ? [this.dice[0], this.dice[0], this.dice[0], this.dice[0]]
                     : [this.dice[0], this.dice[1]];
 
-                this.diceDiv.innerText = (this.dice.join("  "));
                 break;
 
             case "state":
                 const turn = data['payload']['turn'];
                 this.myTurn = turn === this.color;
-                this.turnDiv.innerText = this.myTurn ? "Your turn!" : "Opponent turn";
 
                 this.board = data['payload']['board'];
-                const normalized = this.normalizeBoard(this.board);
-                this.renderBoard(normalized);
                 break;
 
             case "ERROR":
@@ -94,74 +95,13 @@ class Game {
         this.remainingMoves.splice(idx, 1);
         console.log(this.remainingMoves);
 
-        // Păstrăm turul și culoarea
         console.log(`Moved from ${from + 1} to ${to + 1} | Color: ${this.color === 87 ? 'White' : 'Black'}`);
-        this.renderRemainingMoves();
 
         if (!this.remainingMoves.length) {
             setTimeout(() => {
                 this.ws.broadcast({ action: "GAME", type: "advance", payload: {} });
                 console.log(`Turn ended. Waiting for opponent... | Color: ${this.color === 87 ? 'White' : 'Black'}`);
-                this.renderRemainingMoves();
             }, 200);
         }
-    }
-
-    renderRemainingMoves() {
-        const remEl = document.getElementById("remainingMoves");
-        remEl.innerHTML = "Moves left: " + this.remainingMoves.map((d) => `<span style="color: yellow; font-weight: bold;">${d}</span>`).join("  ");
-    }
-    
-    setStatus(text) {
-        this.statusDiv.innerText = text;
-    }
-
-    renderBoard(board) {
-        const boardEl = this.boardDiv;
-        if (!board || board.length !== 24) return;
-
-        const top = board.slice(12, 24);
-        const bottom = board.slice(0, 12).reverse();
-        const rows = 5;
-        let out = "";
-        out += "    13  14  15  16  17  18    19  20  21  22  23  24\n";
-        out += "    ------------------------------------------------\n";
-
-        for (let r = rows - 1; r >= 0; r--) {
-            out += "  |";
-            for (let i = 0; i < 12; i++) {
-                const [type, count] = top[i];
-                out += " " + (count > r ? type : ".") + "  ";
-                if (i === 5) out += "|";
-            }
-            out += "|\n";
-        }
-
-        out += "    ------------------------------------------------\n";
-
-        for (let r = 0; r < rows; r++) {
-            out += "  |";
-            for (let i = 0; i < 12; i++) {
-                const [type, count] = bottom[i];
-                out += " " + (count > r ? type : ".") + "  ";
-                if (i === 5) out += "|";
-            }
-            out += "|\n";
-        }
-
-        out += "    ------------------------------------------------\n";
-        out += "     12  11  10  9   8   7    6   5   4   3   2   1";
-
-        boardEl.innerText = out;
-    }
-
-    normalizeBoard(board) {
-        return board.map(([type, count]) => {
-            let t;
-            if (type === 87) t = "W";
-            else if (type === 66) t = "B";
-            else t = ".";
-            return [t, count];
-        });
     }
 }
